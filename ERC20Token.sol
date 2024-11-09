7c63dce1d37acf3b965711069916dd0274d9aef1
KOSASIH <kosasihg88@gmail.com>
2024-11-09 14:04:38 +0700

    Create ERC20Token.sol

---
+++ ERC20Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20Token is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("NexusToken", "NTK") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
